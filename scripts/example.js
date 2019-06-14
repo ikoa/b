a3a4c41ac9073ca1022a8b66c5b50dbf37321d72
ikoa <a058265@gmail.com>
2019-06-14 15:02:33 +0900

    rename arg

--- scripts/example.js
+++ scripts/example.js
@@ -4,18 +4,18 @@ const service = require('./service');
     module.exports = function(robot) {
 
         // hello world
-        robot.hear(/helo/i, (res) => {
-            res.send("world");
+        robot.hear(/helo/i, (msg) => {
+            msg.send("world");
         });
 
         // sushi
-        robot.hear(/sushi/i, (res) => {
-            res.send(":sushi: SUSHIRO↑ SUSHIRO↓");
+        robot.hear(/sushi/i, (msg) => {
+            msg.send(":sushi: SUSHIRO↑ SUSHIRO↓");
         });
 
         // 定時退社
-        robot.hear(/定時退社/i, (res) => {
-            res.send(service.teiji(res.message.username));
+        robot.hear(/定時退社/i, (msg) => {
+            msg.send(service.teiji(msg.message.username));
         });
 
     };
