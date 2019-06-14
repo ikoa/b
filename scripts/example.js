8fe9431e93be8f90b3a2f707ed67d0ddbbcfb05d
ikoa <a058265@gmail.com>
2019-06-14 18:14:32 +0900

    add express

--- scripts/example.js
+++ scripts/example.js
@@ -1,5 +1,7 @@
 const service = require('./service');
 const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
 
 (function() {
     module.exports = function(robot) {
@@ -23,9 +25,13 @@ const fetch = require('node-fetch');
     };
 }).call(this);
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-fetch('/iron-lung', {method: 'GET'})
-	  .then(res => console.log(res)).catch(err => console.log(err));
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+});
 
-fetch('/')
-    .then((res) => res.text("hello world"));
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
