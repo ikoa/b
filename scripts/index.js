7d4389bd0e65b594d209bd08d86cd94044466e69
aoki <a058265@gmail.com>
2019-06-16 15:51:48 +0900

    delete fetch

--- scripts/index.js
+++ scripts/index.js
@@ -27,13 +27,3 @@ const app = express();
 
     };
 }).call(this);
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-    response.send('Hello World!');
-});
-
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
-});
