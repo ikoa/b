57a827a6d985e14cbadcc31ec1325b786d33dab9
ikoa <a058265@gmail.com>
2019-06-21 13:59:53 +0900

    refactoring(service)

--- scripts/service.js
+++ scripts/service.js
@@ -1,19 +1,8 @@
-// import { Client } from 'pg';
-// const client = new Client({
-//     host: '[データベースのホスト]',
-//     database: '[データベース名]',
-//     user: '[ユーザーID]',
-//     password: '[パスワード]'
-// });
-// client.connect();
-
-module.exports = {
-    teiji: (name) => {
-        return isSuccess() ?
-            ":sexygirl1: < 成功！　　" + " `" + name + "の勝率:" + 0 +"勝" + 0 + "敗`":
-            ":sexygirl1: < 失敗・・・" + " `" + name + "の勝率:" + 0 +"勝" + 0 + "敗`";
-    }
-};
+export function teiji(name) {
+    return isSuccess() ?
+        ":sexygirl1: < 成功！　　" + " `" + name + "の勝率:" + 0 + "勝" + 0 + "敗`" :
+        ":sexygirl1: < 失敗・・・" + " `" + name + "の勝率:" + 0 + "勝" + 0 + "敗`";
+}
 
 const isSuccess = () => {
     return Math.floor(Math.random()*2) === 0; // 0 or 1
